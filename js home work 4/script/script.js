d7edf9977868336c9bd3246415097cd051b63a16
Roman <angelaxel86@gmail.com>
2022-07-08 14:27:59 +0300

    Домашняя работа JS№5 Объекты

--- js home work 4/script/script.js
+++ js home work 4/script/script.js
@@ -1,7 +1,8 @@
 var a='', b='', sign='', res, and = false;
 
-//const out = document.querySelector('.display p');
-
+var out = document.getElementById('paragraph');
+//var out = document.querySelector('.display p');
+console.log(out);
 function add(a=0, b=0) {
 			return (+a) + (+b);
 }
@@ -21,6 +22,7 @@ function division(a=0, b=0) {
 function calculate(a, b , operation) {
 	res = operation(a,b);	
 	document.querySelector('.display p').textContent = res;
+	//out.textContent = res;
 }
 function clearAll() {
 	a = '';
